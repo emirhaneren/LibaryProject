e503d12fa57d47e16edf2585b582993b4fa7f7f2
Emirhan Eren <emirhanneren52@gmail.com>
2023-05-18 16:02:50 +0300

    18.05.2023

--- LibaryProject/Controllers/RegisterController.cs
+++ LibaryProject/Controllers/RegisterController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibaryProject.Models.Entity;
 
 namespace LibaryProject.Controllers
 {
     public class RegisterController : Controller
     {
-        // GET: Register
-        public ActionResult KayitOl()
+		// GET: Register
+#pragma warning disable IDE0044 // Add readonly modifier
+		DbLibaryEntities db = new DbLibaryEntities();
+#pragma warning restore IDE0044 // Add readonly modifier
+        [HttpGet]
+		public ActionResult KayitOl()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult KayitOl(TblUyeler p)
+        {
+            db.TblUyeler.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("KayitOl");
         }
     }
 }
